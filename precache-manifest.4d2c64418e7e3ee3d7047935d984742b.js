da498d0b312feef28bcec88057b47ecc5f045fa4
Isaac Collier <isaacjcollier@gmail.com>
2022-05-22 12:42:25 -0400

    Updates

---
+++ precache-manifest.4d2c64418e7e3ee3d7047935d984742b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "91365057fc019564b2e9",
+    "url": "/Product-Team-Coach/static/css/main.1d08aa52.chunk.css"
+  },
+  {
+    "revision": "91365057fc019564b2e9",
+    "url": "/Product-Team-Coach/static/js/main.91365057.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/Product-Team-Coach/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "b2d4d806d738db43c3b6",
+    "url": "/Product-Team-Coach/static/js/2.b2d4d806.chunk.js"
+  },
+  {
+    "revision": "535eb85b419bf7730d72",
+    "url": "/Product-Team-Coach/static/js/runtime~main.535eb85b.js"
+  },
+  {
+    "revision": "7082c42e59aaf62d82f6e3a284d6b958",
+    "url": "/Product-Team-Coach/static/media/bookexample.7082c42e.png"
+  },
+  {
+    "revision": "e32d04847885ca83a0acc041e7f7ed8c",
+    "url": "/Product-Team-Coach/static/media/profile.e32d0484.jpg"
+  },
+  {
+    "revision": "b6e5c29bbdffa35f7932212cad06bf18",
+    "url": "/Product-Team-Coach/index.html"
+  }
+];
