20bd4407dcfcff02ff79c1c1fd3a43c45e2ffd8d
Isaac Collier <isaacjcollier@gmail.com>
2022-06-02 14:42:45 -0400

    Updates

---
+++ precache-manifest.70dd8b98abb1843c2ae9f564affe6a3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "020ee6a35784cae91ecd",
+    "url": "/static/css/main.97437a2c.chunk.css"
+  },
+  {
+    "revision": "020ee6a35784cae91ecd",
+    "url": "/static/js/main.020ee6a3.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "ef349f98cd42085e4b8b",
+    "url": "/static/js/2.ef349f98.chunk.js"
+  },
+  {
+    "revision": "ed64082d111baeb537c6",
+    "url": "/static/js/runtime~main.ed64082d.js"
+  },
+  {
+    "revision": "5f917fe5d3db0da794f1dffc75175b9d",
+    "url": "/static/media/bookcover.5f917fe5.png"
+  },
+  {
+    "revision": "e32d04847885ca83a0acc041e7f7ed8c",
+    "url": "/static/media/profile.e32d0484.jpg"
+  },
+  {
+    "revision": "8a80dfef4d9cfb20c442935485a9e62f",
+    "url": "/static/media/JordanFullProfile.8a80dfef.png"
+  },
+  {
+    "revision": "a053466c590f8ae883c3c5d74490c240",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Product-Team-Coach/precache-manifest.109b8882e795290c5ad2d6d2ad94f45f.js"
+  "/precache-manifest.70dd8b98abb1843c2ae9f564affe6a3b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Product-Team-Coach/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
